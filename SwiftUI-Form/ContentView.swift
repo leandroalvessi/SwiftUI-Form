c5ea1979648bd04d1cdb4e1072982666751e48ac
Leandro Alves da Silva <leandroalves.desenvolvedor@gmail.com>
2022-06-14 23:23:25 -0300

    #01 - SwiftUI Form TextField, DatePicker, Toggle, Stepper, Link, Sections e Header

--- SwiftUI-Form/ContentView.swift
+++ SwiftUI-Form/ContentView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var primeiroNome = ""
+    @State private var sobreNome = ""
+    @State private var nacimento = Date()
+    @State private var receberNoticias = false
+    @State private var numeroLikes = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("Informações Pessoais")) {
+                    TextField("Nome", text: $primeiroNome)
+                    TextField("Sobrenome", text: $sobreNome)
+                    DatePicker("Nascimento", selection: $nacimento, displayedComponents: .date)
+                }
+                Section(header: Text("Preferências")) {
+                    Toggle("Receber notícias", isOn: $receberNoticias)//.toggleStyle(SwitchToggleStyle(tint: .red))
+                    Stepper("Numero de Likes", value: $numeroLikes, in: 1...100)
+                    Text("Numero de likes \(numeroLikes)")
+                    Link("Linkedin", destination: URL(string: "https://www.linkedin.com/in/leandro-alves-da-silva-845b7b10a/")!)
+                }
+            }
+            .navigationTitle("Cadastro")
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button {
+                        hideKeyBoard()
+                    } label: {
+                        Image(systemName: "keyboard.chevron.compact.down")
+                    }
+                    Button("Salvar", action: saveUser)
+                }
+            }
+        }
+        //.accentColor(.red)
+    }
+    func saveUser() {
+        print("Salvo com sucesso.")
     }
 }
 
@@ -19,3 +53,11 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+#if canImport(UIKit)
+extension View {
+    func hideKeyBoard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif

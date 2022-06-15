23d82e2253abb89a0d8f9326ee7a7bebd8b3a4f4
Leandro Alves da Silva <leandroalves.desenvolvedor@gmail.com>
2022-06-14 22:14:17 -0300

    Initial Commit

---
+++ SwiftUI-Form/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Form
+//
+//  Created by Leandro Alves da Silva on 14/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Form/SwiftUI_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FormApp.swift
+//  SwiftUI-Form
+//
+//  Created by Leandro Alves da Silva on 14/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
